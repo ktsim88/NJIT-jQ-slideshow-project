6fb3df4bf52abd8db0ef3834312728602bf4f523
ktsim88 <526ktsim@frhsd.com>
2025-11-15 18:37:19 +0000

    Implemented next and previous button functionality

--- gallery.js
+++ gallery.js
@@ -66,23 +66,23 @@ function swapPhoto () {
 function showNextPhoto () {
   // Increment mCurrentIndex and call swapPhoto()
   // Ensure it loops back to the beginning if mCurrentIndex exceeds array length
-  // mCurrentIndex++;
-  // swapPhoto();
-  // if (mCurrentIndex > mImages.length) {
-  //   mCurrentIndex = 0;
-  //   swapPhoto();
-  // }
+  mCurrentIndex++;
+  swapPhoto();
+  if (mCurrentIndex > mImages.length) {
+    mCurrentIndex = 0;
+    swapPhoto();
+  }
 }
 
 // Goes to the previous photo, loops to the last photo if mCurrentIndex goes negative
 function showPrevPhoto () {
   // Decrement mCurrentIndex and call swapPhoto()
-  // mCurrentIndex--;
-  // swapPhoto()
-  // // Ensure it loops to the end if mCurrentIndex is less than 0
-  // if (mCurrentIndex > mCurrentIndex.length) {
-  //   mCurrentIndex = 0;
-  //  }
+  mCurrentIndex--;
+  swapPhoto()
+  // Ensure it loops to the end if mCurrentIndex is less than 0
+  if (mCurrentIndex > mCurrentIndex.length) {
+    mCurrentIndex = 0;
+   }
 }
 
 // Starter code for the timer function
